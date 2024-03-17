88298df3c90bf2098c6211d2adb27af545bec701
Ben (Xeon/Arch) <bennyschulenburg@gmx.de>
2024-03-17 16:12:33 +0100

    WIP

--- pong/game.scm
+++ pong/game.scm
@@ -26,11 +26,27 @@
 (define left-player (- (/ win-height 2) (/ player-height 2)))
 (define right-player left-player)
 
+(define ball-x (/ win-width 2))
+(define ball-y (/ win-height 2))
+(define ball-vx 2)
+(define ball-vy 1.2)
+(define ball-width 16)
+(define ball-height 16)
+
 (define ren #f)
 (define tex-bg #f)
 (define tex-ball #f)
 (define tex-player-a #f)
 (define tex-player-b #f)
+
+(define (->int v)
+  (inexact->exact (round v)))
+
+#|
+(define circle-in-square (px py pr bx by bw bh)
+  (cond ((> (+ px pr) bx) #f)
+        ((> (+ px pr) bx) #f)
+|#
 
 (define (clamp v vmin vmax)
   (min (max v vmin) vmax))
@@ -38,12 +54,16 @@
 (define (draw-player x y tex)
   (render-copy ren tex #:dstrect (list x y player-width player-height)))
 
+(define (draw-ball x y)
+  (render-copy ren tex-ball #:dstrect (list (->int (- x (/ ball-width 2))) (->int (- y (/ ball-height 2))) ball-width ball-height)))
+
 (define (draw)
   (set-renderer-draw-color! ren 0 0 0 255)
   (clear-renderer ren)
   (render-copy ren tex-bg)
   (draw-player 16 left-player tex-player-a)
   (draw-player (- win-width (+ player-width 16)) right-player tex-player-b)
+  (draw-ball ball-x ball-y)
   (present-renderer ren)
   (set! angle (+ angle 0.1)))
 
@@ -58,19 +78,23 @@
 
 (define (handle-input)
   (when (key-pressed? 'escape) (set! game-running #f))
-  (when (key-pressed? 'w) (set! left-player (- left-player 1)))
-  (when (key-pressed? 's) (set! left-player (+ left-player 1)))
-  (when (key-pressed? 'up) (set! right-player (- right-player 1)))
-  (when (key-pressed? 'down) (set! right-player (+ right-player 1)))
+  (when (key-pressed? 'w) (set! left-player (- left-player 6)))
+  (when (key-pressed? 's) (set! left-player (+ left-player 6)))
+  (when (key-pressed? 'up) (set! right-player (- right-player 6)))
+  (when (key-pressed? 'down) (set! right-player (+ right-player 6)))
   (set! left-player (clamp left-player 0 (- win-height player-height)))
-  (set! right-player (clamp right-player 0 (- win-height player-height)))
-  (set! iterations (+ 1 iterations)))
+  (set! right-player (clamp right-player 0 (- win-height player-height))))
 
+(define (update-ball)
+  (set! ball-x (+ ball-x ball-vx))
+  (set! ball-y (+ ball-y ball-vy)))
 
 (define (game-loop)
   (when game-running
+    (set! iterations (+ 1 iterations))
     (handle-events)
     (handle-input)
+    (update-ball)
     (draw)
     (yield) ; Necessary so Emacs doesn't block waiting on geiser
     (game-loop)))
@@ -79,8 +103,7 @@
   (set! tex-bg (surface->texture ren (load-image "assets/bg.png")))
   (set! tex-ball (surface->texture ren (load-image "assets/ball.png")))
   (set! tex-player-a (surface->texture ren (load-image "assets/player_a.png")))
-  (set! tex-player-b (surface->texture ren (load-image "assets/player_b.png")))
-  #f)
+  (set! tex-player-b (surface->texture ren (load-image "assets/player_b.png"))))
 
 (define (pong-start options)
   (set! game-running #t)
@@ -88,6 +111,10 @@
   (set! iterations 0)
   (set! left-player (- (/ win-height 2) (/ player-height 2)))
   (set! right-player left-player)
+  (set! ball-x (/ win-width 2))
+  (set! ball-y (/ win-height 2))
+  (set! ball-vx 2)
+  (set! ball-vy 1.2)
   (sdl-init)
   (image-init)
   (call-with-window (make-window)
